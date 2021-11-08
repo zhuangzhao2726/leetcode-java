03495aa3b7ce9e5659710740dbbfa22614140cf3
dubbo <dubbo@xiaohongshu.com>
2021-11-08 21:58:53 +0800

    add zhu

--- src/main/java/com/zhuangzhao/leetcode/Solution.java
+++ src/main/java/com/zhuangzhao/leetcode/Solution.java
@@ -13,3 +13,33 @@ class Solution {
         return Arrays.copyOfRange(points, 0, K);
     }
 }
+
+class Pig {
+    private String name;
+    private String color;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
